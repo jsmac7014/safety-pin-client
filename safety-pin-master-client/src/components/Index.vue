fbe598ca6fb2da89c88d5e2c4025e6392eb9a496
jinhyeokfang <jinhyeokfang@gmail.com>
2018-05-18 20:14:18 +0900

    add Index.vue

--- safety-pin-master-client/src/components/Index.vue
+++ safety-pin-master-client/src/components/Index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-
-  </div>
+    <section>
+        Test
+    </section>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: "Index"    
 }
 </script>
 
