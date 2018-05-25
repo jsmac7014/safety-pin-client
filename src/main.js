c093a30d9fe32fda12e12a92e62de32e49232191
jinhyeokfang <jinhyeokfang@gmail.com>
2018-05-25 22:29:46 +0900

    use vuetify

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import App from './App'
 import axios from 'axios'
 import VueSession from 'vue-session'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 //import VueSocketIO from 'vue-socket.io'
 
 Vue.prototype.$http = axios
 
 //Vue.use(VueSocketIO, 'http://localhost:8080')
 Vue.use(VueSession)
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
